6c6d63bb5e70326d994d043a90a03ba6f4130f95
Theo Debefve <theo.debefve@gmail.com>
2022-12-08 10:44:54 +0100

    :sparkles: Link new widget to stats screen

--- source/sealfm/lib/screens/stats.dart
+++ source/sealfm/lib/screens/stats.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/menu_bar.dart';
+import '../widgets/stats/bottom_menu_widget.dart';
+import '../widgets/stats/stats_card_widget.dart';
+import '../widgets/stats/top_menu_widget.dart';
+
 class StatsScreen extends StatelessWidget {
   const StatsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF5F6FA),
       appBar: AppBar(
         toolbarHeight: 0,
         backgroundColor: Colors.white,
       ),
-      body: (
-
+      body: Center(
+        child: Column(
+          children: [
+            const TopMenu(),
+            Expanded(
+                child: ListView(
+              children: const [
+                Line(),
+                Line(),
+                Line(),
+                Line(),
+                Line(),
+                Line(),
+                Line(),
+              ],
+            )),
+            const BottomMenu(),
+          ],
+        ),
       ),
       bottomNavigationBar: const MenuBar(),
     );
