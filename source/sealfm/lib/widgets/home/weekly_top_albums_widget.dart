f0c8b1c227584a981e90da5973c3617c625e5aca
Theo Debefve <theo.debefve@gmail.com>
2022-12-08 10:34:03 +0100

    :lipstick: Add weekly_top_albums widget

---
+++ source/sealfm/lib/widgets/home/weekly_top_albums_widget.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WeeklyTopAlbumCard extends StatelessWidget {
+  const WeeklyTopAlbumCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var title = 'Weekly Top Albums';
+    var subtitle = 'Belgium';
+
+    var imagePath =
+        'https://i.scdn.co/image/ab67616d00001e022724364cd86bb791926b6cc8';
+
+    var number = '1';
+    var album = 'Civilisation Edition Ultime';
+    var artist = 'Orelsan';
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+            elevation: 4,
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10.0),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              title,
+                              style: GoogleFonts.nunito(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ),
+                            Text(
+                              subtitle,
+                              style: GoogleFonts.nunito(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.black),
+                            ),
+                          ]),
+                    ),
+                    Row(children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12.0),
+                        child: Image.network(imagePath,
+                            width: 60, height: 60, fit: BoxFit.fill),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'n°$number',
+                              style: GoogleFonts.nunito(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              album,
+                              style: GoogleFonts.nunito(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ),
+                            Text(
+                              artist,
+                              style: GoogleFonts.nunito(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.normal,
+                                  color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ]),
+                    Container(
+                      margin: const EdgeInsets.only(top: 10.0),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF0E4DA4),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/top');
+                        },
+                        child: Text(
+                          'See more ...',
+                          style: GoogleFonts.nunito(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
