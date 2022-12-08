33f92592b33c59012ed22664038c533833d8b41c
Theo Debefve <theo.debefve@gmail.com>
2022-12-08 10:41:30 +0100

    :sparkles: Link new widget to top screen

--- source/sealfm/lib/screens/top.dart
+++ source/sealfm/lib/screens/top.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/menu_bar.dart';
+import '../widgets/top/top_close_widget.dart';
+import '../widgets/top/top_open_widget.dart';
 
 class TopScreen extends StatelessWidget {
   const TopScreen({super.key});
@@ -8,13 +10,15 @@ class TopScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF5F6FA),
       appBar: AppBar(
         toolbarHeight: 0,
         backgroundColor: Colors.white,
       ),
-      body: (
-
+      body: ListView(
+        children: const [
+          TopCardClose(),
+          TopCardOpen(),
+        ],
       ),
       bottomNavigationBar: const MenuBar(),
     );
