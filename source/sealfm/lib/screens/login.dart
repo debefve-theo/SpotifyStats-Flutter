d1a29c762d5c0e0a00f0c6f74a05173652240864
Theo Debefve <theo.debefve@gmail.com>
2022-12-08 10:26:49 +0100

    :lipstick: Login screen UI

--- source/sealfm/lib/screens/login.dart
+++ source/sealfm/lib/screens/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sealfm/utilities/icons.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -7,14 +8,44 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF5F6FA),
-      appBar: AppBar(
-        toolbarHeight: 0,
-        backgroundColor: Colors.white,
-      ),
-      body: (
-
-      ),
-    );
+        body: Stack(
+      children: <Widget>[
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/img/login-background.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Center(
+          child: Container(
+            margin: const EdgeInsets.only(right: 10.0),
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/');
+              },
+              icon: const Icon(
+                MyFlutterApp.spotify_blue,
+                color: Color(0xFF0E4DA4),
+              ), //icon data for elevated button spotify_blue
+              label: Text(
+                'Login with Spotify',
+                style: GoogleFonts.nunito(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: const Color(0xFF0E4DA4),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
