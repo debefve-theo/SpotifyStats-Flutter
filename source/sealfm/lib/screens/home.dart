b07747525c84a388bab1d7028e0f75bb7aa803eb
Theo Debefve <theo.debefve@gmail.com>
2022-12-08 10:36:13 +0100

    :sparkles: Link new widget to home screen

--- source/sealfm/lib/screens/home.dart
+++ source/sealfm/lib/screens/home.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/menu_bar.dart';
+import '../widgets/home/profil_card_log.dart';
+import '../widgets/home/profil_card_not_log.dart';
+import '../widgets/home/weekly_top_albums_widget.dart';
+import '../widgets/home/weekly_top_artists_widget.dart';
+import '../widgets/home/weekly_top_songs_widget.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -11,9 +18,16 @@ class HomeScreen extends StatelessWidget {
         toolbarHeight: 0,
         backgroundColor: Colors.white,
       ),
-      body: (
-
+      body: ListView(
+        children: const [
+          ProfilCardLog(),
+          ProfilCard(),
+          WeeklyTopAlbumCard(),
+          WeeklyTopSongsCard(),
+          WeeklyTopArtistsCard()
+        ],
       ),
+      bottomNavigationBar: const MenuBar(),
     );
   }
 }
